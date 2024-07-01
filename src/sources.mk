1ae5b424843892d12fc32efd7f673c41d4b99ac0
dmmontoto <dmontoto@ucsd.edu>
2024-07-01 11:35:51 -0700

    added sources / includes for both builds

--- src/sources.mk
+++ src/sources.mk
@@ -10,8 +10,15 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+SOURCES_COMMON = main.c \
+                 memory.c
+
+SOURCES_MSP432 = interrupts_msp432p401r_gcc.c \
+                 startup_msp432p401r_gcc.c \
+                 system_msp432p401r.c
 
 # Add your include paths to this variable
-INCLUDES = 
+INCLUDES_COMMON = -I../include/common
+
+INCLUDES_MSP432 = -I../include/msp432 -I../include/CMSIS
 
